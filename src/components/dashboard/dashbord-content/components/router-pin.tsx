2a715ba8fdf94f25414cbc7f1dc84925c58187a4
Peter Luis <mail@luis-peter.de>
2025-10-26 20:24:03 +0100

    add copyright notice

--- src/components/dashboard/dashbord-content/components/router-pin.tsx
+++ src/components/dashboard/dashbord-content/components/router-pin.tsx
@@ -1,3 +1,10 @@
+/*
+ * IM_PRJ - Internet Analysis
+ * Copyright (c) 2025 Leitwert GmbH. All rights reserved.
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see LICENSE.txt in the project root.
+ */
+
 import React from 'react';
 import {
     HoverCard,
