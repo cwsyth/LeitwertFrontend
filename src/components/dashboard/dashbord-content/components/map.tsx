46062ad9c80f34178f21fc99f546e305cfac4ec1
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-10-26 21:15:45 +0100

    fix clustering example

--- src/components/dashboard/dashbord-content/components/map.tsx
+++ src/components/dashboard/dashbord-content/components/map.tsx
@@ -1,34 +1,54 @@
 'use client';
 
 import {useRef} from 'react';
-import {Map, Source} from 'react-map-gl/maplibre';
+import {Map, Source, Layer} from 'react-map-gl/maplibre';
+import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
 
-import type {MapRef} from 'react-map-gl/maplibre';
+import type {MapRef, MapMouseEvent} from 'react-map-gl/maplibre';
 import type {GeoJSONSource} from 'maplibre-gl';
 
 export default function DashboardContentMap() {
     const mapRef = useRef<MapRef>(null);
 
+    const onClick = async (event: MapMouseEvent) => {
+        const feature = event?.features?.[0];
+        const clusterId = feature?.properties.cluster_id;
+        const geojsonSource = mapRef?.current?.getSource('earthquakes') as GeoJSONSource;
+        const zoom = await geojsonSource.getClusterExpansionZoom(clusterId);
+
+        if (feature?.geometry && 'coordinates' in feature.geometry) {
+            mapRef?.current?.easeTo({
+                center: feature.geometry.coordinates as [number, number],
+                zoom,
+                duration: 500
+            });
+        }
+    };
+
     return (
         <div className="dashboard-map w-full h-full bg-gradient-to-br from-slate-800 to-slate-900 relative overflow-hidden rounded-[var(--radius)]">
-            <Map
+           <Map
                 initialViewState={{
-                latitude: 40.67,
-                longitude: -103.59,
-                zoom: 3
+                    latitude: 40.67,
+                    longitude: -103.59,
+                    zoom: 3
                 }}
                 mapStyle="https://demotiles.maplibre.org/style.json"
-                onClick={() => {}}
+                interactiveLayerIds={[clusterLayer.id!]}
+                onClick={onClick}
                 ref={mapRef}
             >
                 <Source
-                id="earthquakes"
-                type="geojson"
-                data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
-                cluster={true}
-                clusterMaxZoom={14}
-                clusterRadius={50}
+                    id="earthquakes"
+                    type="geojson"
+                    data="https://maplibre.org/maplibre-gl-js/docs/assets/earthquakes.geojson"
+                    cluster={true}
+                    clusterMaxZoom={14}
+                    clusterRadius={50}
                 >
+                <Layer {...clusterLayer} />
+                <Layer {...clusterCountLayer} />
+                <Layer {...unclusteredPointLayer} />
                 </Source>
             </Map>
         </div>
