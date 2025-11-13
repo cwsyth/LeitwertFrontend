61a0a6cbdbbd498bd7c256e4c1a743596cfa1fdc
Peter Luis <mail@luis-peter.de>
2025-11-13 15:33:31 +0100

    testing pipeline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Lagebild - Dashboard",
+  title: "Lagebild - Dashboard (Dev)",
   description: "description",
 };
 
