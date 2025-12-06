1f804cb6bc9793745d50a2e8437a959ed710a12f
Peter Luis <mail@luis-peter.de>
2025-12-06 13:26:04 +0100

    updated API endpoint

--- src/services/networkApi.ts
+++ src/services/networkApi.ts
@@ -7,7 +7,7 @@
 
 import { CountriesSummaryResponse, CountryAsResponse, NetworkStatus } from '@/types/network';
 
-const API_BASE_URL = process.env.INTERNAL_FRONTEND_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_API_URL;
 
 export const networkApi = {
     async getCountriesSummary(limit: number = 10, status?: NetworkStatus): Promise<CountriesSummaryResponse> {
