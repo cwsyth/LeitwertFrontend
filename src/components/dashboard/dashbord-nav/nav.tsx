2797fc6bf33614fcc87b209bae416e93cc158262
Peter Luis <mail@luis-peter.de>
2025-12-07 21:24:36 +0100

    removed implicit css property "bg-muted"

--- src/components/dashboard/dashbord-nav/nav.tsx
+++ src/components/dashboard/dashbord-nav/nav.tsx
@@ -15,7 +15,7 @@ export default function DashboardNav({ mode, setMode }: DashboardNavProps) {
             type="single"
             value={mode}
             onValueChange={(v) => setMode(v as DashboardContentMode)}
-            className="flex w-full items-center rounded-lg border bg-muted p-1 bg-accent"
+            className="flex w-full items-center rounded-lg border p-1 bg-accent"
         >
             <ToggleGroupItem
                 value="street"
