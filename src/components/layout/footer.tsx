ced891fde6df5043a94127e4374f206135e6f33e
Peter Luis <mail@luis-peter.de>
2025-12-20 15:01:23 +0100

    test

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="main-footer flex items-center justify-center">
         <div className="max-w-[var(--content-max-width)] w-full flex items-center justify-between text-white">
             <span className="bg-white/5 backdrop-blur-md border border-white/10 rounded-xl px-4 py-3 shadow-xl">© 2025 Lagebild</span>
-            <span className="bg-white/5 backdrop-blur-md border border-white/10 rounded-xl px-4 py-3 shadow-xl">Dashboard {version}</span>
+            <span className="bg-white/5 backdrop-blur-md border border-white/10 rounded-xl px-4 py-3 shadow-xl">Test Dashboard {version}</span>
         </div>
     </footer>
     );
