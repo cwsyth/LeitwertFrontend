470b344b0ff68c1d58ddfa06848e39124b297131
Peter Luis <mail@luis-peter.de>
2025-12-21 20:48:51 +0100

    remove unused function

--- src/components/network-table/network-table.tsx
+++ src/components/network-table/network-table.tsx
@@ -119,41 +119,6 @@ export function NetworkTable({
         useSensor(KeyboardSensor, {})
     )
 
-    const getPageNumbers = () => {
-        const pages: (number | 'ellipsis')[] = []
-        const maxVisible = 5
-
-        if (totalPages <= maxVisible) {
-            for (let i = 1; i <= totalPages; i++) {
-                pages.push(i)
-            }
-        } else {
-            if (currentPage <= 3) {
-                for (let i = 1; i <= 4; i++) {
-                    pages.push(i)
-                }
-                pages.push('ellipsis')
-                pages.push(totalPages)
-            } else if (currentPage >= totalPages - 2) {
-                pages.push(1)
-                pages.push('ellipsis')
-                for (let i = totalPages - 3; i <= totalPages; i++) {
-                    pages.push(i)
-                }
-            } else {
-                pages.push(1)
-                pages.push('ellipsis')
-                pages.push(currentPage - 1)
-                pages.push(currentPage)
-                pages.push(currentPage + 1)
-                pages.push('ellipsis')
-                pages.push(totalPages)
-            }
-        }
-
-        return pages
-    }
-
     function handleDragEnd(event: DragEndEvent) {
         const {active, over} = event
 
